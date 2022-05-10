0f86a6b7885daa4cf54e5f7af3b97a7e74914749
Franciscopako <104807581+Franciscopako@users.noreply.github.com>
2022-05-09 21:02:45 -0500

    Create temperatura.py

---
+++ Clase 3/temperatura.py
@@ -0,0 +1,14 @@
+
+grado_actual= int(input("Cual es la Temperatura actual"))
+
+
+if grado_actual in range(25,30):
+    print( "se antoja un pozol")
+
+
+elif grado_actual >= 35:
+    print("ta juerte la calor")
+
+elif grado_actual < 25:
+    print("es hora de sacar el sueter cucarachiento")
+
