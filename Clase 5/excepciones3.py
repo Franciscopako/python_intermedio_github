396bb4684260f709ca386cebbcd9413d484d5df5
Franciscopako <104807581+Franciscopako@users.noreply.github.com>
2022-05-23 18:42:04 -0500

    actividad

--- Clase 5/excepciones3.py
+++ Clase 5/excepciones3.py
@@ -1,12 +1,23 @@
-import os
 # Actividad excepciones
 # Replicar el comportamiento de creacion de carpetas con Windows
-
 # al crear un directorio si ya existe agregarle entre parentesis el consecutivo
 # carpeta
 # carpeta(1)
 # carpeta(2)
 
-contenido = os.listdir('C:\\Users\\tavodev\\Desktop\\python_intermedio_github')
+import os
 
-print(contenido)
+os.system('cls')
+
+Path="C:\\python_intermedio_github\\"
+NomDir="Nueva_Carpeta"
+Lista=os.listdir(Path)
+Lista2=[]
+for Contador in range(len(Lista)):
+    if str(Lista[Contador]).find(NomDir)==0:
+        ParteNumero=str(Lista[Contador])[len(NomDir):len(str(Lista[Contador]))]
+        if (ParteNumero.isnumeric()): Lista2.append(ParteNumero) # Determina el ultimo               
+if Lista2==[]:
+   os.makedirs(Path+NomDir+"1") # Crea el primero si no existe
+else:
+   os.makedirs(Path+NomDir+str(int(max(Lista2))+1)) # Crea el siguiente
--- clase6/peticiones2.py
+++ clase6/peticiones2.py
@@ -5,7 +5,7 @@ os.system('cls')
 
 response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
-response_dic= response.json()
+response_dic = response.json()
 
 for key, value in response_dic.items():
     print(f"key => {key} | value => {value}")
