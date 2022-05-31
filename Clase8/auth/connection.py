12078f6a11f3e3b384a395c867a147f4b280c4a8
Franciscopako <104807581+Franciscopako@users.noreply.github.com>
2022-05-30 20:23:51 -0500

    omitir

--- Clase8/auth/connection.py
+++ Clase8/auth/connection.py
@@ -4,7 +4,7 @@ def get_connection():
     conn = None
     cur = None 
     try:
-        conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=root")
+        conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=123456")
         cur = conn.cursor()
     except Exception as e:
         print(e)
